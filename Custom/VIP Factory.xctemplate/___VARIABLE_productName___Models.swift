aad48684cf6dc72797ab7ae747ca5bb3fb5f1d6e
th1rt3en <1002964+th1rt3en@users.noreply.github.com>
2017-10-12 10:40:44 +0800

    Add models

---
+++ Custom/VIP Factory.xctemplate/___VARIABLE_productName___Models.swift
@@ -0,0 +1,16 @@
+//___FILEHEADER___
+
+import Foundation
+
+enum ___VARIABLE_productName___ {
+	
+	enum Fetch {
+		struct Request {
+		}
+		struct Response {
+		}
+		struct ViewModel {
+		}
+	}
+}
+
